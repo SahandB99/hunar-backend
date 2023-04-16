24e6f092df82cdf88a87e52ecd992a619a4e8d0f
SahandB99 <sahandb25@gmail.com>
2023-04-16 12:13:59 +0300

    edited arts.controller.js added queries

--- controllers/arts.controller.js
+++ controllers/arts.controller.js
@@ -2,7 +2,25 @@ import Art from "../models/arts.models.js";
 
 export const getArts = async (req, res) => {
   try {
-    const arts = await Art.find();
+    let query = JSON.stringify(req.query);
+    query = query.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`);
+
+    let queryObj = JSON.parse(query);
+
+    const excludeQuery = ["sort", "limit", "page", "fields"];
+
+    excludeQuery.forEach((i) => {
+      delete queryObj[i];
+    });
+
+    const getQuery = Art.find(JSON.parse(query));
+
+    if (req.query.sort) {
+      getQuery.sort(req.query.sort);
+    }
+
+    const arts = await getQuery;
+
     res.json({ status: "success", data: arts });
   } catch (err) {
     res.status(400).json({ status: "error", data: err });

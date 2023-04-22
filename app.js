864ea25c0e10387f8cc439090295dc5f7e9ab021
SahandB99 <sahandb25@gmail.com>
2023-04-22 14:10:11 +0300

    edited app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import morgan from "morgan";
 import { connectDb } from "./config/db.js";
 
 import productsRoutes from "./routes/arts.routes.js";
+import userRoutes from "./routes/users.routes.js";
+import "./strategy/auth.js";
 
 import dotenv from "dotenv";
 import { trimQueryMiddleware } from "./middlewares/trimQuery.middleware.js";
@@ -21,5 +23,6 @@ if (process.env.NODE_ENV === "development") {
 }
 
 app.use("/api/products", productsRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;

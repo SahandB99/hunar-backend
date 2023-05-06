2b9b972a59e91c733b0a855cb4b059c5036e107c
SahandB99 <sahandb25@gmail.com>
2023-05-06 20:53:24 +0300

    added trycatch and edited auth.js

--- strategy/auth.js
+++ strategy/auth.js
@@ -1,6 +1,11 @@
 import passport from "passport";
 import { Strategy as localStrategy } from "passport-local";
+import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
+import dotenv from "dotenv";
+
 import Users from "../models/user.model.js";
+
+dotenv.config();
 
 passport.use(
   "signup",
@@ -10,7 +15,7 @@ passport.use(
       passwordField: "password",
       passReqToCallback: true,
     },
-    async (req, email, password, cb) => {
+    async (req, email, password, done) => {
       try {
         const user = await Users.create({
           email,
@@ -18,9 +23,54 @@ passport.use(
           username: req.body.username,
         });
 
-        return cb(null, user);
+        return done(null, user);
       } catch (err) {
-        cb(err);
+        done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  "login",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await Users.findOne({ email });
+
+        if (!user) return done(null, false, { message: "invalid credentials" });
+
+        const validate = await user.isValidPassword(password);
+
+        console.log("user");
+
+        if (!validate)
+          return done(null, false, { message: "invalid credentials" });
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (err) {
+        console.log(err);
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      secretOrKey: process.env.JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (token, done) => {
+      try {
+        return done(null, token.user);
+      } catch (error) {
+        done(error);
       }
     }
   )
